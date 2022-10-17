cd30e85bb9409711ebac0b53533d8dcc7585c821
James Harrington <jamesharrington585@hotmail.com>
2022-10-17 01:43:12 -0400

    added functions

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,28 @@
 var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-// if (topic === 'HTML') {
-//     console.log("Let's study HTML!");
-// } else if (topic === 'CSS') {
-//     console.log("Let's study CSS!");
-// } else if (topic === 'Git') {
-//     console.log("Let's study Git!");
-// } else if (topic === 'Javascript') {
-//     console.log("Let's study Javascript!");
-// } else {
-//     console.log('Please try again!');
-// }
+function listTopics() {
 
+    for (i = 0; i < topics.length; i++) {
+     console.log(topics[i]);
+    }
+ }
 
-for (i = 0; i < topics.length; i++) {
-    console.log(topics[i]);
+function selectTopic() {
+if (randomTopic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (randomTopic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (randomTopic === 'Git') {
+    console.log("Let's study Git!");
+} else if (randomTopic === 'Javascript') {
+    console.log("Let's study Javascript!");
+} else {
+    console.log('SPlease try again!');
 }
+}
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log("Which topic should we study first?");
+selectTopic();
